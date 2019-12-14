cff071d0f026f08a19718ec1bebf86e5bf5bf8be
nildiert <niljordan23@gmail.com>
2019-12-14 14:47:04 -0500

    - Created tasks_controller - Created tasks_index view - Added resources to tasks in routes file - Added default value to status column in the task table - Created migration for tasks

--- db/migrate/20191214193345_create_tasks.rb
+++ db/migrate/20191214193345_create_tasks.rb
@@ -2,7 +2,7 @@ class CreateTasks < ActiveRecord::Migration[5.2]
   def change
     create_table :tasks do |t|
       t.string :name
-      t.boolean :status
+      t.boolean :status, :default => false
 
       t.timestamps
     end
