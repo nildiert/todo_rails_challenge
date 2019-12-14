661ba3a4686de4e31f92a78cd84928dc38f2f115
nildiert <niljordan23@gmail.com>
2019-12-14 15:18:43 -0500

    Added controller methods

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,13 +1,43 @@
 class TasksController < ApplicationController
     def index
+      @tasks = Task.all
     end
-
+  
     def create
+      @task = Task.new(task_params)
+      if @task.save
+        redirect_to root_path
+      else
+        redirect_to root_path
+      end
     end
-
+  
+  
     def update
+      @task = Task.find(params[:id])
+      if @task.status
+        @task.status = false
+      else
+        @task.status = true
+      end
+       
+      @task.save
+      redirect_to root_path
     end
-
+  
+  
     def destroy
+      @task = Task.find(params[:id])
+      @task.destroy
+      redirect_to root_path
     end
-end
+  
+  
+    private
+  
+    def task_params
+      params.require(:task).permit(:name)
+    end
+  
+  
+  end
