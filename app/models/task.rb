5e7da8e48fed606a2015e7b14c68727b6f035fca
nildiert <niljordan23@gmail.com>
2019-12-15 15:16:24 -0500

    Added conditional in view to check if the user hav tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,7 @@
 class Task < ApplicationRecord
+
+    scope :completed_tasks, -> { where(status: true) }
+    scope :incompleted_tasks, -> { where(status: false) }
+
     validates :name, presence: true
 end
