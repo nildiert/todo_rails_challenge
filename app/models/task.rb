0aedd1836a1329f7083fbba5edb5b740ca17a1a3
nildiert <niljordan23@gmail.com>
2019-12-15 15:43:14 -0500

    Added validation tests and scope tests

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,17 @@
 class Task < ApplicationRecord
+    before_create :default_values
 
     scope :completed_tasks, -> { where(status: true) }
     scope :incompleted_tasks, -> { where(status: false) }
 
+    validates :status, :inclusion => { :in => [true, false] }
     validates :name, presence: true
+
+    
+  
+    protected
+  
+    def default_values
+      self.status ||= false
+    end  
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  context 'validation tests' do
+
+    it 'ensures name presence' do
+      task = Task.new(status: false).save
+      expect(task).to eq(false)
+    end
+
+    it 'should default status to false' do
+      task = Task.new(name: 'new task')
+      task.status.should == false
+    end
+
+    it 'should save successfully presence' do
+      task = Task.new(name: 'new task').save
+      expect(task).to eq(true)
+    end
+
+  end
+  
+  context 'scope tests' do
+    
+    let (:params) { {name: 'task'} }
+    before(:each) do
+      Task.new(params).save
+      Task.new(params).save
+      Task.new(params.merge(status: true)).save
+      Task.new(params.merge(status: false)).save
+      Task.new(params.merge(status: false)).save
+    end
+
+    it 'should return completed tasks' do
+      expect(Task.completed_tasks.size).to eq(1)
+    end
+
+    it 'should return incompleted tasks' do
+      expect(Task.incompleted_tasks.size).to eq(4)
+    end
+  end
 end
